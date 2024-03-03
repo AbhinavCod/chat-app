22a2b2b4387f76915dc24f64d83479c4fe26b1a7
AbhinavCod <you@example.com>
2024-03-03 15:34:18 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ const __dirname = path.resolve();
 const port = process.env.PORT || 5000;
 
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
-}));
+// app.use(cors({
+//     origin:"http://localhost:3000",
+//     credentials:true
+// }));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
