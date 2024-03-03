792c0283f44cd8f5cded8a1473c446c4981a4a47
AbhinavCod <you@example.com>
2024-03-03 16:12:31 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
 
-server.listen(port,()=>{
+server.listen(5000,()=>{
     connection();
     console.log(`Server is running at port ${port}`);
 })
